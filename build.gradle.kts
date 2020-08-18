36f63540ce68a38c29f55406c99ccf558700a98b
jmp <jasonpenilla2@me.com>
2020-08-17 17:31:38 -0700

    switch to building with gradle

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+plugins {
+    `java-library`
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+val projectName = "jmplib"
+group = "xyz.jpenilla"
+version = "1.0.0+{BUILD_NUMBER}-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven(url = "https://oss.sonatype.org/content/groups/public/")
+    maven(url = "https://repo.extendedclip.com/content/repositories/placeholderapi")
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    annotationProcessor("org.projectlombok", "lombok", "1.18.12")
+    compileOnly("org.projectlombok", "lombok", "1.18.12")
+    compileOnly("org.jetbrains", "annotations", "20.0.0")
+    compileOnly("org.spigotmc", "spigot-api", "1.13.2-R0.1-SNAPSHOT")
+    compileOnly("com.github.jmanpenilla", "adventure-text-minimessage", "2cb5077c9d")
+    compileOnly("net.kyori", "adventure-platform-bukkit", "4.0.0-SNAPSHOT")
+    compileOnly("net.kyori", "adventure-text-feature-pagination", "4.0.0-SNAPSHOT")
+    compileOnly("me.clip", "placeholderapi", "2.10.9")
+    compileOnly("com.github.DiamondDagger590", "Prisma", "a622d01b80")
+}
+
+val autoRelocate by tasks.register<ConfigureShadowRelocation>("configureShadowRelocation", ConfigureShadowRelocation::class) {
+    target = tasks.getByName("shadowJar") as ShadowJar?
+    val packageName = "${project.group}.${project.name.toLowerCase()}"
+    prefix = "$packageName.shaded"
+}
+
+tasks {
+    withType<ShadowJar> {
+        archiveClassifier.set("")
+        archiveFileName.set("$projectName-${project.version}.jar")
+        dependsOn(autoRelocate)
+        minimize()
+    }
+}
