8c6bfeccaccf562b92adf226908315f0c3a54e6c
jmp <jasonpenilla2@me.com>
2020-09-04 17:21:13 -0700

    b

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 
 dependencies {
     annotationProcessor("org.projectlombok", "lombok", "1.18.12")
-    api("com.github.jmanpenilla", "adventure-text-minimessage", "2cb5077c9d")
+    api("com.github.jmanpenilla", "adventure-text-minimessage", "test-SNAPSHOT")
     api("net.kyori", "adventure-platform-bukkit", "4.0.0-SNAPSHOT")
     api("net.kyori", "adventure-text-feature-pagination", "4.0.0-SNAPSHOT")
     compileOnly("org.projectlombok", "lombok", "1.18.12")
