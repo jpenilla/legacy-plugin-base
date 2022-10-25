7f9fab4a35952e9200fcb23432c7de5ed46e7ea0
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-10-25 16:22:10 -0700

    Add PaperComponentUtil.fromNative

--- src/main/java/xyz/jpenilla/pluginbase/legacy/PaperComponentUtil.java
+++ src/main/java/xyz/jpenilla/pluginbase/legacy/PaperComponentUtil.java
@@ -13,15 +13,18 @@ public final class PaperComponentUtil {
     private static final String KYORI_PREFIX = String.join(".", "net", "kyori");
     private static final @Nullable Object NATIVE_GSON;
     private static final @Nullable Method NATIVE_DESERIALIZE;
+    private static final @Nullable Method NATIVE_SERIALIZE;
 
     static {
         if (!Environment.paper()) {
             NATIVE_GSON = null;
             NATIVE_DESERIALIZE = null;
+            NATIVE_SERIALIZE = null;
         } else {
             try {
                 final Class<?> gsonClass = Class.forName(String.join(".", "net", "kyori", "adventure", "text", "serializer", "gson", "GsonComponentSerializer"));
                 NATIVE_DESERIALIZE = gsonClass.getMethod("deserializeFromTree", JsonElement.class);
+                NATIVE_SERIALIZE = gsonClass.getMethod("serializeToTree", nativeAdventureComponentClass());
                 NATIVE_GSON = gsonClass.getDeclaredMethod("gson").invoke(null);
             } catch (final ReflectiveOperationException ex) {
                 throw new RuntimeException(ex);
@@ -41,6 +44,21 @@ public final class PaperComponentUtil {
         }
         try {
             return NATIVE_DESERIALIZE.invoke(NATIVE_GSON, GsonComponentSerializer.gson().serializeToTree(component));
+        } catch (final ReflectiveOperationException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static Component fromNative(final Object component) {
+        if (Component.class.getName().startsWith(KYORI_PREFIX)) {
+            return (Component) component;
+        }
+        if (NATIVE_GSON == null || NATIVE_SERIALIZE == null) {
+            throw new IllegalStateException();
+        }
+        try {
+            return GsonComponentSerializer.gson().deserializeFromTree((JsonElement) NATIVE_SERIALIZE.invoke(NATIVE_GSON, component));
         } catch (final ReflectiveOperationException ex) {
             throw new RuntimeException(ex);
         }
