2bac41bb595f9ddee9ae93e6acdc2c829bfdb660
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-10-11 13:31:21 -0700

    Add release constants

---
+++ src/main/java/xyz/jpenilla/pluginbase/legacy/environment/MinecraftReleases.java
@@ -0,0 +1,57 @@
+package xyz.jpenilla.pluginbase.legacy.environment;
+
+import static xyz.jpenilla.pluginbase.legacy.environment.MinecraftRelease.minecraftRelease;
+
+public final class MinecraftReleases {
+    private MinecraftReleases() {
+    }
+
+    public static final MinecraftRelease v1_12 = minecraftRelease(12, 0);
+    public static final MinecraftRelease v1_12_1 = minecraftRelease(12, 1);
+    public static final MinecraftRelease v1_12_2 = minecraftRelease(12, 2);
+    public static final MinecraftRelease v1_13 = minecraftRelease(13, 0);
+    public static final MinecraftRelease v1_13_1 = minecraftRelease(13, 1);
+    public static final MinecraftRelease v1_13_2 = minecraftRelease(13, 2);
+    public static final MinecraftRelease v1_14 = minecraftRelease(14, 0);
+    public static final MinecraftRelease v1_14_1 = minecraftRelease(14, 1);
+    public static final MinecraftRelease v1_14_2 = minecraftRelease(14, 2);
+    public static final MinecraftRelease v1_14_3 = minecraftRelease(14, 3);
+    public static final MinecraftRelease v1_14_4 = minecraftRelease(14, 4);
+    public static final MinecraftRelease v1_15 = minecraftRelease(15, 0);
+    public static final MinecraftRelease v1_15_1 = minecraftRelease(15, 1);
+    public static final MinecraftRelease v1_15_2 = minecraftRelease(15, 2);
+    public static final MinecraftRelease v1_16 = minecraftRelease(16, 0);
+    public static final MinecraftRelease v1_16_1 = minecraftRelease(16, 1);
+    public static final MinecraftRelease v1_16_2 = minecraftRelease(16, 2);
+    public static final MinecraftRelease v1_16_3 = minecraftRelease(16, 3);
+    public static final MinecraftRelease v1_16_4 = minecraftRelease(16, 4);
+    public static final MinecraftRelease v1_16_5 = minecraftRelease(16, 5);
+    public static final MinecraftRelease v1_17 = minecraftRelease(17, 0);
+    public static final MinecraftRelease v1_17_1 = minecraftRelease(17, 1);
+    public static final MinecraftRelease v1_18 = minecraftRelease(18, 0);
+    public static final MinecraftRelease v1_18_1 = minecraftRelease(18, 1);
+    public static final MinecraftRelease v1_18_2 = minecraftRelease(18, 2);
+    public static final MinecraftRelease v1_19 = minecraftRelease(19, 0);
+    public static final MinecraftRelease v1_19_1 = minecraftRelease(19, 1);
+    public static final MinecraftRelease v1_19_2 = minecraftRelease(19, 2);
+    public static final MinecraftRelease v1_19_3 = minecraftRelease(19, 3);
+    public static final MinecraftRelease v1_19_4 = minecraftRelease(19, 4);
+    public static final MinecraftRelease v1_20 = minecraftRelease(20, 0);
+    public static final MinecraftRelease v1_20_1 = minecraftRelease(20, 1);
+    public static final MinecraftRelease v1_20_2 = minecraftRelease(20, 2);
+    public static final MinecraftRelease v1_20_3 = minecraftRelease(20, 3);
+    public static final MinecraftRelease v1_20_4 = minecraftRelease(20, 4);
+    public static final MinecraftRelease v1_20_5 = minecraftRelease(20, 5);
+    public static final MinecraftRelease v1_20_6 = minecraftRelease(20, 6);
+    public static final MinecraftRelease v1_21 = minecraftRelease(21, 0);
+    public static final MinecraftRelease v1_21_1 = minecraftRelease(21, 1);
+    public static final MinecraftRelease v1_21_2 = minecraftRelease(21, 2);
+    public static final MinecraftRelease v1_21_3 = minecraftRelease(21, 3);
+    public static final MinecraftRelease v1_21_4 = minecraftRelease(21, 4);
+    public static final MinecraftRelease v1_21_5 = minecraftRelease(21, 5);
+    public static final MinecraftRelease v1_21_6 = minecraftRelease(21, 6);
+    public static final MinecraftRelease v1_21_7 = minecraftRelease(21, 7);
+    public static final MinecraftRelease v1_21_8 = minecraftRelease(21, 8);
+    public static final MinecraftRelease v1_21_9 = minecraftRelease(21, 9);
+    public static final MinecraftRelease v1_21_10 = minecraftRelease(21, 10);
+}
--- src/main/java/xyz/jpenilla/pluginbase/legacy/environment/MinecraftVersion.java
+++ src/main/java/xyz/jpenilla/pluginbase/legacy/environment/MinecraftVersion.java
@@ -9,4 +9,18 @@ public interface MinecraftVersion {
         }
         return this instanceof MinecraftSnapshot; // Always count snapshots as latest
     }
+
+    default boolean isOlderThan(final MinecraftVersion other) {
+        if (this instanceof MinecraftRelease && other instanceof MinecraftRelease) {
+            return ((MinecraftRelease) this).compareTo((MinecraftRelease) other) < 0;
+        }
+        return other instanceof MinecraftSnapshot && this instanceof MinecraftRelease; // Releases are older than snapshots
+    }
+
+    default boolean isOrOlder(final MinecraftVersion other) {
+        if (this instanceof MinecraftRelease && other instanceof MinecraftRelease) {
+            return ((MinecraftRelease) this).compareTo((MinecraftRelease) other) <= 0;
+        }
+        return this instanceof MinecraftRelease; // Releases are always older than or equal to snapshots
+    }
 }
